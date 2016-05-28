52c14c2bd8f1299a7bd05fee50aa0f8c46ee6114
lbl <205858588@qq.com>
2016-05-28 20:03:08 +0800

    by win10

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("大家好,这是我第一次在GitHub发表呈现!");
+        System.out.println("大家好,这是我第一次在GitHub发表程序啊!");
     }
 }
